805c134a123578c1a52708cdc77e608589fe2f0f
Tori Champagne <champagnet@moravian.edu>
2026-04-21 09:32:35 -0400

    update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install git -y
 git clone https://github.com/cs220s26/champagne_cicd /champagne_cicd
-cd /<repo_name>
+cd /champagne_cicd
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
 cp web_counter.service /etc/systemd/system
